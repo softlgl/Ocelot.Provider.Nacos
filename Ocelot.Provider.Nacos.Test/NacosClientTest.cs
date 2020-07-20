d237b1da57ecddea06ef1f064dce1ba4540074be
Mr Li <yanzi@yanzideMacBook-Pro.local>
2020-07-20 16:17:24 +0800

    添加测试文件

---
+++ Ocelot.Provider.Nacos.Test/NacosClientTest.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Provider.Nacos.NacosClient;
+using Xunit;
+
+namespace Ocelot.Provider.Nacos.Test
+{
+    public class NacosClientTest
+    {
+        [Fact]
+        public async void TestClient()
+        {
+            IServiceCollection services = new ServiceCollection();
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            services.AddNacosDiscovery(configurationBuilder.Build());
+
+            var provider = services.BuildServiceProvider();
+            StatusReportBgTask statusReportBgTask = provider.GetRequiredService<StatusReportBgTask>();
+            await statusReportBgTask.StartAsync();
+            Console.ReadLine();
+        }
+    }
+}
