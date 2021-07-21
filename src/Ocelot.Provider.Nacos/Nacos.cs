7589f8541c77132cb9ee4d3cabc64ceda1a1d875
liguoliang <ligl@icbf.com.cn>
2021-07-21 15:45:30 +0800

    修复自定义GroupName&ClusterName发现不了的bug

--- src/Ocelot.Provider.Nacos/Nacos.cs
+++ src/Ocelot.Provider.Nacos/Nacos.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Ocelot.ServiceDiscovery.Providers;
 using Ocelot.Values;
 using Nacos.V2;
+using Microsoft.Extensions.Options;
+using Ocelot.Provider.Nacos.NacosClient.V2;
+using NacosConstants = Nacos.V2.Common.Constants;
 
 namespace Ocelot.Provider.Nacos
 {
@@ -12,18 +15,23 @@ namespace Ocelot.Provider.Nacos
     {
         private readonly INacosNamingService _client;
         private readonly string _serviceName;
+        private readonly string _groupName;
+        private readonly List<string> _clusters;
 
-        public Nacos(string serviceName, INacosNamingService client)
+        public Nacos(string serviceName, INacosNamingService client, IOptions<NacosAspNetOptions> options)
         {
-            _client = client;
             _serviceName = serviceName;
+            _client = client;
+            _groupName = string.IsNullOrWhiteSpace(options.Value.GroupName) ? 
+                NacosConstants.DEFAULT_GROUP : options.Value.GroupName;
+            _clusters = (string.IsNullOrWhiteSpace(options.Value.ClusterName) ? NacosConstants.DEFAULT_CLUSTER_NAME : options.Value.ClusterName).Split(",").ToList();
         }
 
         public async Task<List<Service>> Get()
         {
             var services = new List<Service>();
 
-            var instances = await _client.GetAllInstances(_serviceName);
+            var instances = await _client.GetAllInstances(_serviceName, _groupName, _clusters);
 
             if (instances != null && instances.Any())
             {
